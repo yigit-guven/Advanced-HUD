70f3fb0be89a625e8928e7ddccb44ee418242b61
Yiğit Güven <yigitguven0124@gmail.com>
2026-02-03 05:56:56 +0100

    Initial Fabric Version Setup

---
+++ src/client/java/com/yigit/advancedhud/AdvancedHudClient.java
@@ -0,0 +1,10 @@
+package com.yigit.advancedhud;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AdvancedHudClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
